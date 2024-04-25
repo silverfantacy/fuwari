c84a28089ddf5b9ab3274129194658ae5540bfd5
Zero <silverfantacy@gmail.com>
2024-04-25 09:30:14 +0800

    fix: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,7 +24,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://laplusda.com/",
   base: "/",
   integrations: [
     tailwind(),
